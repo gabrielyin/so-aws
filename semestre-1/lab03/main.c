a02a8933fbd5f0f98be42843de80987bdcba145e
Gabriel Yin <2021gabrielyin@gmail.com>
2024-03-18 15:33:22 -0300

    Create main.c for lab03

---
+++ semestre-1/lab03/main.c
@@ -0,0 +1,27 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int count = 100;
+
+static void *code_for_thread1(void *arg) {
+  count = 40;
+  printf("Valor da variável na thread alterado para  %d\n", count);
+  return NULL;
+}
+
+int main(void) {
+  count = 100;
+  printf("Valor da variável global no processo pai antes da criação da thread: %d\n", count);
+
+  pthread_t thread1;
+
+  pthread_create(&thread1, NULL, code_for_thread1, NULL);
+  pthread_join(thread1, NULL);
+
+  printf("Valor da variável global no processo pai após a criação da thread: %d\n", count);
+
+  exit(0);
+}
