d36a59c3e1ac1e276284de587fd144b58a19cbfe
Gabriel Yin <2021gabrielyin@gmail.com>
2024-04-16 07:26:48 -0300

    Update main.c

--- semestre-1/proj1/main.c
+++ semestre-1/proj1/main.c
@@ -1,5 +1,6 @@
 #include <pthread.h>
 #include <stdio.h>
+#include <unistd.h>
 
 typedef struct {
   int time;
@@ -8,26 +9,60 @@ typedef struct {
 
 int globalTime = 0;
 int direction = 0;
+int amountPeople = 0;
+int endTime = 0;
 
 void *person(void *arg) {
-  Person *person = (Person *)arg;
+  Person *people = (Person *)arg;
 
-  if (direction != person->direction) {
-    
+  direction = people[0].direction;
+  endTime = people[0].time + 10;
+
+  int counter = 0;
+  int endCounter = 0;
+  Person waiting;
+
+  while (1) {
+    if (globalTime == endTime) {
+      if (direction == 1) {
+        direction = 0;
+      } else {
+        direction = 1;
+      }
+
+      if (waiting.direction == direction) {
+        endTime = endTime + 10;
+        endCounter++;
+      }
+    }
+
+    if (globalTime == people[counter].time) {
+      if (direction == people[counter].direction) {
+        if (people[counter].time <= endTime) {
+          endTime = people[counter].time + 10;
+          endCounter++;
+        }
+        counter++;
+      } else {
+        waiting = people[counter];
+        counter++;
+      }
+    }
+
+    globalTime++;
+    if (endCounter == amountPeople) {
+      break;
+    }
   }
-
-  return NULL;
 }
 
 int main() {
-
   FILE *file = fopen("entrada.txt", "r");
   if (file == NULL) {
     printf("Erro ao abrir arquivo de entrada.\n");
     return 1;
   }
 
-  int amountPeople;
   if (fscanf(file, "%d", &amountPeople) != 1) {
     printf("Erro ao ler número de pessoas");
     fclose(file);
@@ -44,24 +79,20 @@ int main() {
     }
   }
 
-  pthread_t threads[amountPeople];
-  for (int i = 0; i < amountPeople; i++) {
-    if (pthread_create(&threads[i], NULL, person, &people[i]) != 0) {
-      printf("Erro ao criar thread para a pessoa %d.\n", i + 1);
-      fclose(file);
-      return 1;
-    }
+  pthread_t threads;
+  if (pthread_create(&threads, NULL, person, &people) != 0) {
+    printf("Erro ao criar thread para a pessoa %d.\n");
+    fclose(file);
+    return 1;
   }
 
-  for (int i = 0; i < amountPeople; i++) {
-    if (pthread_join(threads[i], NULL) != 0) {
-      printf("Erro ao aguardar thread da pessoa %d.\n", i + 1);
-      fclose(file);
-      return 1;
-    }
+  if (pthread_join(threads, NULL) != 0) {
+    printf("Erro ao aguardar thread da pessoa.\n");
+    fclose(file);
+    return 1;
   }
 
-  printf("%d", globalTime);
+  printf("%d", endTime);
 
-  return 1;
+  return 0;
 }
