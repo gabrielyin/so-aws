97318dfd849f0872a3f966ab414034141f946db9
Gabriel Yin <2021gabrielyin@gmail.com>
2024-04-28 17:48:31 -0300

    Update main.c

--- semestre-1/lab04/main.c
+++ semestre-1/lab04/main.c
@@ -1,11 +1,11 @@
 #include <pthread.h>
 #include <stdio.h>
-#define ROWS 2
+#define ROWS 3
 #define COLS 3
 
-int matrix[ROWS][COLS] = {{1, 2, 3}, {4, 5, 6}};
-int vector[ROWS] = {7, 1};
-int result[ROWS] = {0, 0};
+int matrix[ROWS][COLS] = {{1, 2, 3}, {4, 5, 6}, {1, 2, 3}};
+int vector[ROWS] = {7, 1, 1};
+int result[ROWS] = {0, 0, 0};
 
 void *multiply(void *arg) {
   int i = *((int *)arg);
@@ -21,18 +21,18 @@ int main() {
   pthread_t threads[ROWS];
   int i, thread_args[ROWS];
 
-  for (i = 0; i < 2; i++) {
+  for (i = 0; i < ROWS; i++) {
     thread_args[i] = i;
     pthread_create(&threads[i], NULL, multiply, (void *)&thread_args[i]);
   }
 
-  for (i = 0; i < 2; i++) {
+  for (i = 0; i < ROWS; i++) {
     pthread_join(threads[i], NULL);
   }
 
   printf("--- Resultado ---\n");
-  for (i = 0; i < 2; i++) {
-    printf("[%d]: %d\n",i, result[i]);
+  for (i = 0; i < ROWS; i++) {
+    printf("[%d]: %d\n", i, result[i]);
   }
 
   return 0;
